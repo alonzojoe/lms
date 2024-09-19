f9f7089558917b4e2f8a54ed14e5cae9107a8ea6
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-09-19 10:26:35 +0800

    feat: Setup pages layout: navbar and sidebar + outlet

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,8 +1,34 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "@/layouts/components/Navbar";
+import Sidebar from "@/layouts/components/Sidebar";
 
 const RootLayout = () => {
-  return <Outlet />;
+  return (
+    <div className="layout-wrapper layout-content-navbar">
+      <div className="layout-container">
+        {/* Sidebar */}
+        <Sidebar />
+        {/* / Sidebar */}
+        <div className="layout-page">
+          {/* Navbar */}
+          <Navbar />
+          {/* Navbar */}
+          {/* Content wrapper */}
+          <div className="content-wrapper">
+            <div className="flex-grow-1 container-p-y container-fluid my-0">
+              <Outlet />
+            </div>
+            {/* Footer 
+         Footer */}
+            <div className="content-backdrop fade"></div>
+          </div>
+          {/* Content wrapper */}
+        </div>
+      </div>
+      <div className="layout-overlay layout-menu-toggle"></div>
+      <div className="drag-target"></div>
+    </div>
+  );
 };
 
 export default RootLayout;
---
+++ src/layouts/components/Sidebar.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside
+      id="layout-menu"
+      className="layout-menu menu-vertical bg-menu-theme"
+      style={{
+        touchAction: "none",
+        userSelect: "none",
+        WebkitUserDrag: "none",
+        WebkitTapHighlightColor: "rgba(0, 0, 0, 0)",
+      }}
+    >
+      <div className="app-brand demo py-1 d-flex align-items-center">
+        <div>
+          <span className="app-brand-logo demo">
+            {/* <img
+              src="../../assets/logos/camarin-logo-white.png"
+              className="img-fluid"
+              height="60"
+              width="60"
+              alt="cdh-logo"
+            /> */}
+          </span>
+          <span className="app-brand-text demo menu-text fw-bold mx-auto">
+            LMS
+          </span>
+        </div>
+        <a
+          href="javascript:void(0);"
+          className="layout-menu-toggle menu-link text-large ms-auto mb-2"
+        >
+          <div className="demo-vertical-spacing btn-switch">
+            <div className="has-error">
+              <label className="switch">
+                <input
+                  type="checkbox"
+                  className="switch-input switch-input"
+                  true-value={true}
+                  false-value={false}
+                />
+                <span className="switch-toggle-slider">
+                  <span className="switch-on"></span>
+                  <span className="switch-off"></span>
+                </span>
+                <span className="switch-label"></span>
+              </label>
+            </div>
+          </div>
+          <i className="ti ti-x d-block d-xl-none ti-sm align-middle btn-x"></i>
+        </a>
+      </div>
+      <div className="menu-inner-shadow"></div>
+      <ul className="menu-inner py-1">
+        <li className="menu-header small text-uppercase">
+          <span className="menu-header-text">Home</span>
+        </li>
+        <li className="menu-item">
+          <a href="javascript:void(0);" className="menu-link">
+            <i className="menu-icon tf-icons"></i>
+            <div>label</div>
+          </a>
+        </li>
+        <li className="menu-header small text-uppercase">
+          <span className="menu-header-text">Auth</span>
+        </li>
+        <li className="menu-item">
+          <a href="javascript:void(0);" className="menu-link">
+            <i className="menu-icon tf-icons ti ti-logout"></i>
+            <div data-i18n="logout">Logout</div>
+          </a>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
