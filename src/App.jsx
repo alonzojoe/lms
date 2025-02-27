6c2543dc99c918605f1195136ef8904be79180d2
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-02-27 11:21:02 +0800

    feat: create new ProctedtedRoutes and Auth component and add changes in App Routing.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RootLayout from "@/layouts/RootLayout";
 import Dashboard from "@/pages/Dashboard/Dashboard";
 import Borrowers from "@/pages/Borrowers/Borrowers";
+import ProtectedRoutes from "@/middleware/ProtectedRoutes";
+import Auth from "@/pages/Auth/Auth";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<RootLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="borrowers" element={<Borrowers />} />
+        <Route path="" element={<Auth />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="home" element={<RootLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="borrowers" element={<Borrowers />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
