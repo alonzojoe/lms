aad1a7b51efdd9beb57f0c98b739f19416be3b05
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-02-27 14:12:30 +0800

    style: create ui ux for Authentication Page

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -2,8 +2,152 @@ import React from "react";
 
 const Auth = () => {
   return (
-    <div>
-      <h2>Login</h2>
+    <div
+      className="authentication-wrapper authentication-cover authentication-bg"
+      style={{
+        height: `100dvh`,
+      }}
+    >
+      <div className="authentication-inner row">
+        <div className="d-none d-lg-flex align-items-center col-lg-7 p-0">
+          <div className="auth-cover-bg auth-cover-bg-color d-flex justify-content-center align-items-center">
+            <img
+              src="../../assets/img/illustrations/auth-login-illustration-light.png"
+              alt="auth-login-cover"
+              className="img-fluid my-5 auth-illustration"
+              data-app-light-img="illustrations/auth-login-illustration-light.png"
+              data-app-dark-img="illustrations/auth-login-illustration-dark.png"
+            />
+
+            <img
+              src="../../assets/img/illustrations/bg-shape-image-light.png"
+              alt="auth-login-cover"
+              className="platform-bg"
+              data-app-light-img="illustrations/bg-shape-image-light.png"
+              data-app-dark-img="illustrations/bg-shape-image-dark.png"
+            />
+          </div>
+        </div>
+
+        <div className="d-flex col-12 col-lg-5 align-items-center p-sm-5 p-4">
+          <div className="w-px-400 mx-auto">
+            <div className="app-brand mb-4">
+              <a href="index.html" className="app-brand-link gap-2">
+                {/* <span className="app-brand-logo demo">
+                  <svg
+                    width="32"
+                    height="22"
+                    viewBox="0 0 32 22"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
+                      fill="#7367F0"
+                    ></path>
+                    <path
+                      opacity="0.06"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
+                      fill="#161616"
+                    ></path>
+                    <path
+                      opacity="0.06"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
+                      fill="#161616"
+                    ></path>
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
+                      fill="#7367F0"
+                    ></path>
+                  </svg>
+                </span> */}
+              </a>
+            </div>
+
+            <h3 className="mb-1 fw-bold">Welcome to System Name! 👋</h3>
+            <p className="mb-4">Please sign-in to your account.</p>
+
+            <form
+              id="formAuthentication"
+              className="mb-3 fv-plugins-bootstrap5 fv-plugins-framework"
+              action="index.html"
+              method="POST"
+              noValidate="novalidate"
+            >
+              <div className="mb-3 fv-plugins-icon-container">
+                <label htmlFor="email" className="form-label">
+                  Email or Username
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="email"
+                  name="email-username"
+                  autoFocus=""
+                />
+                <div className="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
+              </div>
+              <div className="mb-3 form-password-toggle fv-plugins-icon-container">
+                <div className="d-flex justify-content-between">
+                  <label className="form-label" htmlFor="password">
+                    Password
+                  </label>
+                  {/* <a href="auth-forgot-password-cover.html">
+                    <small>Forgot Password?</small>
+                  </a> */}
+                </div>
+                <div className="input-group input-group-merge has-validation">
+                  <input
+                    type="password"
+                    id="password"
+                    className="form-control"
+                    name="password"
+                    aria-describedby="password"
+                  />
+                  <span className="input-group-text cursor-pointer">
+                    <i className="ti ti-eye-off"></i>
+                  </span>
+                </div>
+                <div className="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
+              </div>
+              <div className="mb-3">
+                <div className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    id="remember-me"
+                  />
+                  <label className="form-check-label" htmlFor="remember-me">
+                    {" "}
+                    Remember Me{" "}
+                  </label>
+                </div>
+              </div>
+              <button className="btn btn-primary d-grid w-100 waves-effect waves-light">
+                Sign in
+              </button>
+              <input type="hidden" />
+            </form>
+
+            <p className="text-center">
+              <span>New on our platform?</span>{" "}
+              <a href="auth-register-cover.html">
+                <span>Create an account</span>
+              </a>
+            </p>
+
+            <div className="divider my-4"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
